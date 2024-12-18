327fd25d17a6e38c326750e51fcad00412c79241
Johnny <jonathanbc2012@gmail.com>
2024-12-18 19:14:02 +0100

    feat: react-router-dom installed. ProjectDetails.jsx created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Navbar from "./pages/NavBar/Navbar";
+import ProjectDetails from "./pages/ProjectDetails/ProjectDetails";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/project/:id" element={<ProjectDetails />} />
+      </Routes>
     </>
   );
 }
