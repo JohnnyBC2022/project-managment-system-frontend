87935c21d6e79dbdf225939ab4e9386f94b87174
Johnny <jonathanbc2012@gmail.com>
2024-12-20 20:27:55 +0100

    feat: navigate added to navbar

--- src/pages/NavBar/Navbar.jsx
+++ src/pages/NavBar/Navbar.jsx
@@ -8,12 +8,20 @@ import {
 import CreateProjectForm from "../Project/CreateProjectForm";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { PersonIcon } from "@radix-ui/react-icons";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+
+  const navigate=useNavigate()
+
   return (
     <div className="border-b py-4 px-5 flex items-center justify-between">
       <div className="flex items-center gap-3">
-        <p className="cursor-pointer">Gestión de Proyectos</p>
+        <p
+          className="cursor-pointer"
+          onClick={()=>navigate("/")}
+        >Gestión de Proyectos
+        </p>
         <Dialog>
           <DialogTrigger>
             <Button>Añadir Proyecto</Button>
