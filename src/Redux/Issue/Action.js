5d82ec6fb47f8a470b882df0a0ee86524e4e58b0
Johnny <jonathanbc2012@gmail.com>
2024-12-27 13:29:07 +0100

    feat: issue actions created

---
+++ src/Redux/Issue/Action.js
@@ -0,0 +1,83 @@
+import * as actionTypes from "./ActionTypes"
+import api from "@/config/api"
+
+export const fetchIssues = (projectId) => {
+  return async (dispatch) => {
+    dispatch({ type: actionTypes.FETCH_ISSUES_REQUEST });
+    try {
+      const response = await api.get(`/api/issues/project/${projectId}`);
+      console.log("obteniendo tareas", response.data);
+      dispatch({
+        type: actionTypes.FETCH_ISSUES_SUCCESS,
+        issues: response.data,
+      });
+    } catch (error) {
+      console.log("error --", error);
+      dispatch({
+        type: actionTypes.FETCH_ISSUES_FAILURE,
+        error: error.message,
+      });
+    }
+  };
+};
+
+export const fetchIssuesById = (issueId) => {
+    return async (dispatch) => {
+      dispatch({ type: actionTypes.FETCH_ISSUES_BY_ID_REQUEST });
+      try {
+        const response = await api.get(`/api/issues/${issueId}`);
+        console.log("obteniendo tarea por id", response.data);
+        dispatch({
+          type: actionTypes.FETCH_ISSUES_BY_ID_SUCCESS,
+          issues: response.data,
+        });
+      } catch (error) {
+        console.log("error --", error);
+        dispatch({
+          type: actionTypes.FETCH_ISSUES_BY_ID_FAILURE,
+          error: error.message,
+        });
+      }
+    };
+};
+
+export const updateIssueStatus = ({issueId, status}) => {
+    return async (dispatch) => {
+      dispatch({ type: actionTypes.UPDATE_ISSUE_STATUS_REQUEST });
+      try {
+        const response = await api.put(`/api/issues/${issueId}/status/${status}`);
+        console.log("actualizando estado de la tarea", response.data);
+        dispatch({
+          type: actionTypes.UPDATE_ISSUE_STATUS_SUCCESS,
+          issues: response.data,
+        });
+      } catch (error) {
+        console.log("error --", error);
+        dispatch({
+          type: actionTypes.UPDATE_ISSUE_STATUS_FAILURE,
+          error: error.message,
+        });
+      }
+    };
+};
+
+export const assignedUserToIssue = ({issueId, userId}) => {
+    return async (dispatch) => {
+      dispatch({ type: actionTypes.ASSIGNED_ISSUE_TO_USER_REQUEST });
+      try {
+        const response = await api.put(`/api/issues/${issueId}/assignee/${userId}`);
+        console.log("tarea asignada a usuario", response.data);
+        dispatch({
+          type: actionTypes.ASSIGNED_ISSUE_TO_USER_SUCCESS,
+          issues: response.data,
+        });
+      } catch (error) {
+        console.log("error --", error);
+        dispatch({
+          type: actionTypes.ASSIGNED_ISSUE_TO_USER_FAILURE,
+          error: error.message,
+        });
+      }
+    };
+};
+
