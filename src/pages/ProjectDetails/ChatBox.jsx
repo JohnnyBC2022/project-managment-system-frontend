4ffa30d8d51b3d7c8e87ff6eefe6edd96a82dd5a
Johnny <jonathanbc2012@gmail.com>
2025-01-07 20:16:51 +0100

    feat: chatbox changed

--- src/pages/ProjectDetails/ChatBox.jsx
+++ src/pages/ProjectDetails/ChatBox.jsx
@@ -2,7 +2,11 @@ import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { fetchChatByProject, fetchChatMessages, sendMessage } from "@/Redux/Chat/Action";
+import {
+  fetchChatByProject,
+  fetchChatMessages,
+  sendMessage,
+} from "@/Redux/Chat/Action";
 import { PaperPlaneIcon } from "@radix-ui/react-icons";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -10,41 +14,56 @@ import { useParams } from "react-router-dom";
 
 const ChatBox = () => {
   const [message, setMessage] = useState("");
-  const {auth, chat}=useSelector(store=>store)
-  const {id}=useParams()
-  const dispatch=useDispatch()
+  const { auth, chat } = useSelector((store) => store);
+  const { id } = useParams();
+  const dispatch = useDispatch();
 
-  useEffect(()=>{
-    dispatch(fetchChatByProject(id))
-  },[])
 
-  useEffect(()=>{
-    dispatch(fetchChatMessages(chat.chat?.id))
-  },[])
+  useEffect(() => {
+    dispatch(fetchChatByProject(id));
+  }, [id, dispatch]);
 
-  
+  useEffect(() => {
+    if (chat.chat?.id) {
+      dispatch(fetchChatMessages(chat.chat.id));
+    }
+  }, [chat.chat?.id, dispatch]);
 
   const handleSendMessage = () => {
-    dispatch(sendMessage({
-      senderId: auth.user?.id,
-      projectId:id,
-      content:message
-    }))
-    setMessage("")
-    console.log("mensaje", message)
+    if (!message.trim()) return;
+    dispatch(
+      sendMessage({
+        senderId: auth.user?.id,
+        projectId: id,
+        content: message,
+      })
+    );
+    setMessage("");
+    console.log("mensaje", message);
   };
 
   const handleMessageChange = (e) => {
-    setMessage(e.target.value)
+    setMessage(e.target.value);
   };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      console.log(e.key)
+      handleSendMessage();
+    }
+  };
+
+  
 
   return (
     <div className="sticky">
       <div className="border rounded-lg">
         <h1 className="border-b p-5">Chat</h1>
-        <ScrollArea className="h-[32rem] w-full p-5 flex gap-3 flex-col">
+        <ScrollArea
+        className="h-[32rem] w-full p-5 flex gap-3 flex-col">
           {chat.messages?.map((item, index) =>
-            item.sender.id !==auth.user.id ? (
+            item.sender.id !== auth.user.id ? (
               <div className="flex gap-2 mb-2 justify-start" key={item.id}>
                 <Avatar>
                   <AvatarFallback>J</AvatarFallback>
@@ -57,8 +76,8 @@ const ChatBox = () => {
             ) : (
               <div className="flex gap-2 mb-2 justify-end" key={item}>
                 <div className="space-y-2 py-2 px-5 border rounded-se-2xl rounded-s-xl">
-                <p>{item.sender.fullName}</p>
-                <p className="text-gray-300">{item.content}</p>
+                  <p>{item.sender.fullName}</p>
+                  <p className="text-gray-300">{item.content}</p>
                 </div>
                 <Avatar>
                   <AvatarFallback>J</AvatarFallback>
@@ -71,11 +90,14 @@ const ChatBox = () => {
           <Input
             value={message}
             onChange={handleMessageChange}
+            onKeyDown={handleKeyDown}
+            rows="2"
             className="py-7 border-t outline-none focus:outline-none focus:ring-0 rounded-none border-b-0 border-x-0"
             placeholder="Escribe tu mensaje..."
           />
           <Button
             onClick={handleSendMessage}
+            type="submit"
             className="absolute right-2 top-3 rounded-full"
             size="icon"
           >
