7894f05f11d6a5cd51d7af46caa6f290b18162d3
Johnny <jonathanbc2012@gmail.com>
2025-01-13 21:38:43 +0100

    api changed

--- src/config/api.js
+++ src/config/api.js
@@ -5,7 +5,10 @@ export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 const api = axios.create({ baseURL: API_BASE_URL, withCredentials: true });
 const jwt = localStorage.getItem("jwt");
 
-api.defaults.headers.common["Authorization"] = `Bearer ${jwt}`;
-api.defaults.headers.post["Content-Type"] = "application/json";
+if (jwt) {
+    api.defaults.headers.common["Authorization"] = `Bearer ${jwt}`;
+  } else {
+    api.defaults.headers.post["Content-Type"] = "application/json";
+  }
 
 export default api;
